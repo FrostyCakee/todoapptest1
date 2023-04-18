377d47923b9edd43133ca9bb8672b4b71dc2cf64
FrostyCakee <a.prostokvashin@bk.ru>
2023-04-19 00:53:17 +0200

    init

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -13,11 +13,14 @@
         on:change={() => toggleTodoCompleted(todo.id, todo.completed)}
         class="mr-2 form-checkbox h-5 w-5"/>
 
+        
         <span class={`flex-1 text-gray-800 font-bold ${todo.completed ? 'line-through' : ''}`}>{todo.text}</span>
+        <span class={`flex-1 text-gray-800`}>{todo.due_date}</span>
         <button
         type="button"
         class="text-sm bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         on:click={() => deleteTodo(todo.id)}>Delete</button>
+        
 
         <div><span class={`flex-1 text-gray-500 ml-4`}>Id: {todo.id}</span></div>
     </div>
